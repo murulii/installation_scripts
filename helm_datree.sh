1f7a982d02544e06003bff5234b336b537f34135
murulii <muruli800@gmail.com>
2023-05-02 12:29:00 +0530

    Update helm_datree.sh

--- helm_datree.sh
+++ helm_datree.sh
@@ -12,3 +12,27 @@ curl https://get.datree.io | /bin/bash
 datree version
 datree test <your_kubernetes_manifest_YAML_NAME>
 
+
+
+######jenkins code######
+
+     stage('check misconfig') {
+            steps {
+                
+                script{
+                    
+                    dir('kubernetes/myapp') {
+                        withEnv(['DATREE_TOKEN=2299500f-7284-4362-b8fc-fb1df99caa3b']) {
+                            
+                        sh 'helm datree test .'
+                                            }
+                    }
+                      }
+                
+                  }
+                           }
+        
+        
+       
+        }
+
